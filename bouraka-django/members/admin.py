c93f56df6a594cb33ef87efef4b26bd05aad8b82
Elyes Ighilaza <elyes.ighilaza@gmail.com>
2015-09-28 00:00:49 +0200

    registered member to admin

--- bouraka-django/members/admin.py
+++ bouraka-django/members/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from members.models import Member
 
-# Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+    list_display('name')
+
+admin.site.register(Member)
+#,MemberAdmin)
