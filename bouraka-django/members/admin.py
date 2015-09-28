3d6df91bd117c1cf0e42f55adfc02b15403324c0
Elyes Ighilaza <elyes.ighilaza@gmail.com>
2015-09-28 14:27:40 +0200

    fixed typo in memebers admin

--- bouraka-django/members/admin.py
+++ bouraka-django/members/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from members.models import Member
 
 class MemberAdmin(admin.ModelAdmin):
-    list_display('name')
+    list_display = ('name')
 
 admin.site.register(Member)
 #,MemberAdmin)
