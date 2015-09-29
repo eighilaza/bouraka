468386207dc5ed8d51f0102599c388fbde13e90c
Elyes Ighilaza <elyes.ighilaza@gmail.com>
2015-09-29 16:39:38 +0200

    debugged sponsors view

--- bouraka-django/sponsors/views.py
+++ bouraka-django/sponsors/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from sponsors.models import Sponsor
 
 def index(request):
-    sponsors_list = Sponsors.objects.order_by('order')
+    sponsors_list = Sponsor.objects.order_by('order')
     groups_list = []
     for sponsor in sponsors_list:
         if sponsor.group not in groups_list:
-            groups.append(sponsor.group)
+            groups_list.append(sponsor.group)
     groups_list.sort()
     template = 'sponsors/sponsors.html'
-    context = { 'sponsorsi_list': sponsors_list, 'groups_list': groups_list }
+    context = { 'sponsors_list': sponsors_list, 'groups_list': groups_list }
     return render(request, template, context)
