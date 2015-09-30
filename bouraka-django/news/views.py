e889ca865bb609179619aa19bc8b64631a95809f
Elyes Ighilaza <elyes.ighilaza@gmail.com>
2015-09-30 09:51:13 +0200

    debugged news template

--- bouraka-django/news/views.py
+++ bouraka-django/news/views.py
@@ -3,6 +3,6 @@ from news.models import News
 
 def index(request):
   news_list = News.objects.order_by('-publication_date')
-  template = loader.get_template('news/news.html')
+  template = 'news/news.html'
   context = { 'news_list': news_list }
   return render(request, template, context)
