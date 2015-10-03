3ba398ca08f568ff459a6ee86d8fb8079d5dc2c0
Elyes Ighilaza <elyes.ighilaza@gmail.com>
2015-10-03 12:36:44 +0200

    fixed typo in members model

--- bouraka-django/members/models.py
+++ bouraka-django/members/models.py
@@ -4,7 +4,7 @@ class Member(models.Model):
     name = models.CharField( max_length = 150, blank = True )
     title = models.CharField( max_length = 150, blank = True )
     team = models.CharField( max_length = 150, blank = True )
-    departement = models.CharField( max_length = 150, blank = True  )
+    department = models.CharField( max_length = 150, blank = True  )
     year = models.IntegerField( blank = True )
     description = models.TextField( blank = True )
     photo = models.ImageField( upload_to = 'members' )
