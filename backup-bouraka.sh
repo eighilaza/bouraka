e6dcc277221c5efdada38e070c6ca99b68d04f0d
Elyes Ighilaza <elyes.ighilaza@gmail.com>
2015-10-03 13:01:31 +0200

    remove migrations folders

--- backup-bouraka.sh
+++ backup-bouraka.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Put this file on the machine where you would like to keep the backup
+#And run it periodically using crontab
+
 username="username"
 host="hostname"
 path_to_bouraka="/path/to/bouraka"
--- restart.sh
+++ restart.sh
@@ -5,5 +5,6 @@ docker rm -f bouraka_bourakaweb_1 bouraka_bourakadb_1
 rm -rf bouraka-static/*
 rm -rf bouraka-media/*
 rm -rf bouraka-db/*
+rm -rf bouraka-django/*/migrations
 
 docker-compose up
