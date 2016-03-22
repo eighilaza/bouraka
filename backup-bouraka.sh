143daa0ad9d6f176229a53c9b820d892216304bc
Elyes Ighilaza <elyes.ighilaza@gmail.com>
2016-03-22 14:13:09 +0800

    modified the list of files to back up

--- backup-bouraka.sh
+++ backup-bouraka.sh
@@ -11,4 +11,4 @@ path_to_local_backup="/path/to/local/backup"
 rsync -azv --progress $username@$host:$path_to_bouraka/bouraka-db $path_to_local_backup
 rsync -azv --progress $username@$host:$path_to_bouraka/bouraka-static $path_to_local_backup
 rsync -azv --progress $username@$host:$path_to_bouraka/bouraka-media $path_to_local_backup
-rsync -azv --progress $username@$host:$path_to_bouraka/bouraka-django $path_to_local_backup
+rsync -azv --progress $username@$host:$path_to_bouraka/start-bouraka.sh $path_to_local_backup
