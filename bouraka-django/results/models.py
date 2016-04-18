74bf87f198f591db3a6e90223eef9e99f1cb4776
Elyes Ighilaza <elyes.ighilaza@gmail.com>
2016-04-18 17:50:49 +0800

    fixed bug in results model

--- bouraka-django/results/models.py
+++ bouraka-django/results/models.py
@@ -3,6 +3,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Result(models.Model):
   year = models.PositiveIntegerField(validators=[MinValueValidator(1900),MaxValueValidator(2200)])
-  description = models.CharField(max_length = 500)
+  description = models.TextField(max_length = 500)
   def __str__(self):
     return str(self.year)
